e8341999a3767faf1566f217443323e3d620688b
oleksandr.tilha <oleksandr.tilha@nordstrom.com>
2023-12-07 18:09:46 +0200

    merge

--- src/app/features/courses/courses.module.ts
+++ src/app/features/courses/courses.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { CoreModule } from '@core/core.module';
 import { CoursesComponent } from './courses.component';
-import { ListComponent } from './components/list/list.component';
 import { HeaderListComponent } from './components/header-list/header-list.component';
 import { FooterListComponent } from './components/footer-list/footer-list.component';
 import { CreationDateDirective } from './directives/creation-date.directive';
@@ -8,7 +8,7 @@ import { DurationPipe } from './pipes/duration/duration.pipe';
 import { OrderByPipe } from './pipes/order-by/order-by.pipe';
 import { FilterPipe } from './pipes/filter/filter.pipe';
 import { ItemListComponent } from './components/item-list/item-list.component';
-import { CoreModule } from '@core/core.module';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
